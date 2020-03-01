dfb82db09a8b8a9bab19ea2cd1e58f2d7a71c451
Alex Abbott <alexabbott@Giannis-MacBook-Pro-2.local>
2020-02-29 18:00:22 -0800

    Adjusts favorite styling

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -14,6 +14,7 @@ export class FavoriteComponent implements OnInit {
   @Input() id: string;
   @Input() collection: string;
   @Input() favoritesCount: number;
+  @Input() disabledFavorite: boolean;
 
   constructor(
     public globalService: GlobalService,
